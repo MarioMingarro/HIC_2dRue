305b67e1585d4c8db22cd020890358e2728f8645
Mario Mingarro <60926549+MarioMingarro@users.noreply.github.com>
2023-03-01 14:05:59 +0100

    new

--- Semana_2_1.R
+++ Semana_2_1.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(stringr)
 
-Data <- read.csv2("A:/GABRIEL/RESULTADOS/RESULTADOS_AN.txt", encoding="UTF-8")
+Data <- read.csv2("E:/GABRIEL/RESULTADOS/RESULTADOS_AN.txt", encoding="UTF-8")
 
 Data$S_NAT3_c_1 <- str_replace(Data$S_NAT3_c_1 , ".*_", "")
 Data <- Data[,c(4,13,14,34,28,25,40)]
@@ -53,6 +53,61 @@ Data$RUE_CODE <- replace(Data$RUE_CODE, Data$RUE_CODE == "Maduro", "8")
 Data$RUE_CODE <- replace(Data$RUE_CODE, Data$RUE_CODE == "De referencia", "9")
 Data$RUE_CODE <- replace(Data$RUE_CODE, Data$RUE_CODE == "Anomalias de alto rendimiento", "10")
 
+
+rm(Definicion_THIC)
+rm(THIC)
+# Wilcoson test
+
+ii <- unique(Data$THIC)
+jj <- c("Anomaias de bajo rendimiento","Basal","Muy degradado","Degradado","Productivo de baja biomasa",
+         "Productivo de alta biomasa","Submaduro","Maduro","De referencia","Anomalias de alto rendimiento")
+
+Tabla <- data.frame(THIC = character(),
+                    RUE = character(),
+                    p = numeric())
+
+for (i in c(1,2,4,5,6,7,8,9,10,11)){
+  sel <- dplyr::filter(Data, Data$THIC == ii[i])
+  Tabla_1 <- data.frame(THIC= character(),
+                        RUE = character(),
+                        p = numeric())
+  
+  for (j in 1:length(RUE)){
+    aa <- dplyr::filter(sel, sel$RUE == jj[j])
+    try({
+      WT <- wilcox.test(AREA_km ~ FIGURA, data = aa,
+                      exact = FALSE)
+      })
+    Tabla_2 <- data.frame(THIC = "A",
+                          RUE = "A",
+                          p = 2)
+    Tabla_2[1,1] <- ii[i]
+    Tabla_2[1,2] <- jj[j]
+    Tabla_2[1,3] <- WT$p.value
+    Tabla_1 <- rbind(Tabla_1, Tabla_2)
+    
+  }
+  Tabla <- rbind(Tabla, Tabla_1)
+}
+Tabla$p <-round(Tabla$p, 4)
+
+
+Tabla_n <-  Tabla %>% 
+  pivot_wider(
+    names_from = THIC, 
+    values_from = p
+  )
+write.csv(Tabla_n, "E:/GABRIEL/RESULTADOS/wilcoxon_THIC.csv")
+
+ss <- Data[,c(2,10,6,8)]
+ss$THIC <- as.character(ss$CHFE50)
+tabla_kk <- ss %>%  
+  pivot_wider(
+    names_from = THIC, 
+    values_from = AREA_km,
+    values_fn = mean
+  )
+write.csv(tabla_kk, "E:/GABRIEL/RESULTADOS/areas_THIC.csv")
 
 prop.table(Data$AREA_km)
 
