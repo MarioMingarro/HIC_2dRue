d3ff08b04f873077697f9fe70d742208853a09d8
Mario Mingarro <60926549+MarioMingarro@users.noreply.github.com>
2023-03-16 14:58:36 +0100

    new

--- chi2_test.R
+++ chi2_test.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(stringr)
-library(pwrss)
 
-Data <- read.csv2("D:/GABRIEL/RESULTADOS/NEW/THIC_RN_2dRUE_AN_2.txt", encoding="UTF-8")
+
+Data <- read.csv2("E:/GABRIEL/RESULTADOS/NEW/THIC_RN_2dRUE_AN_2.txt", encoding="UTF-8")
 
 
 Data <- Data[,c(44,50,54,55)]
@@ -10,7 +10,7 @@ colnames(Data) <- c("THIC", "FIGURA", "RUE", "AREA_m")
 Data$AREA_km <- cbind(Data$AREA/1000000)
 9320
 9340
-aa <- dplyr::filter(Data, THIC == "9320")
+aa <- dplyr::filter(Data, THIC == "9340")
 
 kk <- aa %>% 
   group_by(RUE, FIGURA) %>% 
@@ -25,9 +25,19 @@ kk$`No protegido` <- as.numeric(kk$`No protegido`)
 kk$RN2000 <- as.numeric(kk$RN2000 )
 
 chisq <- chisq.test(kk)
-chisq2 <- chisq.test(kk, simulate.p.value = TRUE)
-
-chisq$observed
-chisq$expected
+chisq$p.value
+chisq$residuals
 library(corrplot)
 corrplot(chisq$residuals, is.cor = FALSE)
+
+contrib <- 100*chisq$residuals^2/chisq$statistic
+round(contrib, 3)
+
+corrplot(contrib, is.cor = FALSE)
+
+
+library(RColorBrewer)
+counts <- table(kk)
+barplot(counts, 
+        col=brewer.pal(n = 2, name = "Set2"), 
+        legend = rownames(counts), las = 2)
