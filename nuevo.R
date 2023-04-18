c8a712f0636ff86b72260d0e012140b50122569d
Mario Mingarro <60926549+MarioMingarro@users.noreply.github.com>
2023-04-18 12:49:33 +0200

    new

--- nuevo.R
+++ nuevo.R
@@ -1,80 +1,109 @@
 # Carga los datos
 library(readr)
 library(tidyverse)
-library(MASS)
+
 
 datos <- read_delim("D:/GABRIEL/NUEVO/2dRUE_ZEC_NO_AP_25.txt", 
                             ";", escape_double = FALSE, 
                             locale = locale(decimal_mark = ",", grouping_mark = "."), 
                             trim_ws = TRUE)
 
-kk <- datos %>% 
-  filter(FIGURA== "ZEC")
+ZEC <- datos %>% 
+  filter(FIGURA == "ZEC")
 
-kk2 <- datos %>% 
-  filter(FIGURA== "NP")
+NP <- datos %>% 
+  filter(FIGURA == "NP")
 
 var(kk$gridcode)
 var(kk2$gridcode)
 
-hist(kk$gridcode)
-hist(kk2$gridcode)
+hist(ZEC$gridcode)
+hist(NP$gridcode)
 
-anova
-chi2 con los estados del RUE
+#anova
+#chi2 con los estados del RUE
 
+porcentaje_por_valor <- prop.table(table(NP$gridcode)) * 100
 
+SR_b <- sum(porcentaje_por_valor[6:10])
+SR_a <- sum(porcentaje_por_valor[3:5])
 
-
-
-
-kk <- datos %>% 
-  filter(FIGURA== "ZEC") 
-
-datos %>% 
-  filter(FIGURA== "ZEC") %>% 
-  filter(gridcode == "1") %>% 
-  summarise(n())
-
-porcentaje_por_valor <- prop.table(table(kk$gridcode)) * 100
-
-alto <- mean(porcentaje_por_valor[6:10])
-bajo <- mean(porcentaje_por_valor[3:5])
-
-SER <- (alto-bajo)/(alto+bajo)
+SER <- (SR_b-SR_a)/(SR_b+SR_a)
 
 
-var(kk$gridcode)
 # Crear un dataframe para almacenar los resultados
 resultados <- data.frame()
 
 # Realizar múltiples selecciones aleatorias del tamaño muestral
-n_replicas <- 10000 # Número de replicas
-for (i in 10:n_replicas) {
-  muestra <- sample_n(kk, i, replace = TRUE)
+
+for (i in 10:10000) {
+  muestra <- sample_n(ZEC, i, replace = TRUE)
   
   porcentaje_por_valor <- prop.table(table(muestra$gridcode)) * 100
-  alto <- mean(porcentaje_por_valor[6:10])
-  bajo <- mean(porcentaje_por_valor[3:5])
+  SR_b <- sum(porcentaje_por_valor[6:10])
+  SR_a <- sum(porcentaje_por_valor[3:5])
   
-  SER <- (alto-bajo)/(alto+bajo)
+  SER <- (SR_b-SR_a)/(SR_b+SR_a)
   resultados <- rbind(resultados, data.frame(Replica = i, SER = SER))
 }
 
-porcentaje_por_valor <- prop.table(table(kk2$gridcode)) * 100
-alto <- mean(porcentaje_por_valor[6:10])
-bajo <- mean(porcentaje_por_valor[3:5])
+porcentaje_por_valor <- prop.table(table(NP$gridcode)) * 100
+SR_b <- sum(porcentaje_por_valor[6:10])
+SR_a <- sum(porcentaje_por_valor[3:5])
 
-SER_G <- (alto-bajo)/(alto+bajo)
+SER_G <- (SR_b-SR_a)/(SR_b+SR_a)
+
+
+# Crear un dataframe para almacenar los resultados
+resultados_2 <- data.frame()
+
+# Realizar múltiples selecciones aleatorias del tamaño muestral
+
+for (i in 10:10000) {
+  muestra <- sample_n(ZEC, i, replace = FALSE)
+  
+  porcentaje_por_valor <- prop.table(table(muestra$gridcode)) * 100
+  SR_b <- sum(porcentaje_por_valor[6:10])
+  SR_a <- sum(porcentaje_por_valor[3:5])
+  
+  SER <- (SR_b-SR_a)/(SR_b+SR_a)
+  resultados_2 <- rbind(resultados_2, data.frame(Replica_2 = i, SER_2 = SER))
+}
+
 # Generar el gráfico de caja y bigotes
 ggplot(resultados, aes(x = Replica, y = SER)) +
-  geom_point(alpha= .2, size = 0.5) +
-  geom_hline(yintercept = SER_G, col = "red") +
+  geom_point(alpha= .2, size = 0.5, col = "blue") +
+  geom_point(data=resultados_2, aes(x = Replica_2, y = SER_2), alpha= .2, size = 0.5, col = "red")+
+  geom_hline(yintercept = SER_G, col = "black") +
   scale_x_continuous(breaks = c(10, 1000, 5000, 10000))+
   labs(x = "nº pixeles", y = "SER") +
-  ggtitle("Estabilización del SER en Aleatorización del Tamaño Muestral") +
+  ggtitle("Estabilización del SER con reemplazo (azul) y sin reemplazo (rojo)") +
   theme_classic()
 
 #################################
 
-sd(SER_G)
+resultados <- data.frame()
+
+# Realizar múltiples selecciones aleatorias del tamaño muestral
+
+for (i in 10:10000) {
+  muestra <- sample_n(ZEC, i, replace = TRUE)
+  
+  varianza <- var(muestra$gridcode)
+  
+  resultados <- rbind(resultados, data.frame(Replica = i, Varianza = varianza))
+}
+
+varianza_g <- var(ZEC$gridcode)
+
+
+ggplot(resultados, aes(x = Replica, y = Varianza)) +
+  geom_point(alpha= .2, size = 0.5, col = "blue") +
+  geom_hline(yintercept = varianza_g, col = "black") +
+  scale_x_continuous(breaks = c(10, 1000, 5000, 10000))+
+  labs(x = "nº pixeles", y = "Varianza") +
+  ggtitle("Estabilización de varianza") +
+  theme_classic()
+
+
+
